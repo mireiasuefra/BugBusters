51e19ce9c91cd80614b15917239e4e062a267294
Arnau Gonzalez <arnaugonzalez>
2024-02-20 18:07:26 +0100

    Implementación cargarDatosCSV

--- src/datos/RepositorioJuegosImpl.java
+++ src/datos/RepositorioJuegosImpl.java
@@ -1,11 +1,42 @@
 package datos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import model.Juego;
+
 public class RepositorioJuegosImpl implements RepositorioJuegos {
+
+	private final List<Juego> listado = new ArrayList<Juego>();
 
 	@Override
 	public void cargarDatosCSV() {
-		// TODO Auto-generated method stub
-		
+
+		try (Scanner scanner = new Scanner(new File("res/juegos.csv"))) {
+			scanner.nextLine();
+			while (scanner.hasNextLine())
+				listado.add(leerJuegoString(scanner.nextLine()));
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage() + "res/juegos.csv not found");
+		}
+	}
+
+	private Juego leerJuegoString(String linea) {
+
+		String[] datosJuego = linea.split(",");
+
+		Juego juego = new Juego();
+
+		juego.setNombre(datosJuego[1]);
+		juego.setPlataforma(datosJuego[2]);
+		juego.setFechaPublicacion(datosJuego[3]);
+		juego.setGenero(datosJuego[4]);
+		juego.setEditor(datosJuego[5]);
+
+		return juego;
 	}
 
 }
