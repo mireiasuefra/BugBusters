8945d59ea36488d1145463555e8d0e42ce2a33af
mireiasuefra <mireia.suefra@gmail.com>
2024-02-21 08:18:29 +0100

    añadido enumerado de genero y de plataforma

--- src/model/Platform.java
+++ src/model/Platform.java
@@ -2,4 +2,18 @@ package model;
 
 public enum Platform {
 
+	WII("Wii"), NES("NES"), GB("GB"), DS("DS"), X360("X360"), PS3("PS3"), PS2("PS2"), SNES("SNES"), GBA("SNES"),
+	_3DS("3DS"), PS4("PS4"), N64("N65"), PS5("PS5"), PC("PC"), _2600("2600"), PSP("PSP"), XONE("XOne"),
+	GC("GC"), WIIU("WiiU"), GEN("GEN");
+
+	private final String plataforma;
+
+	private Platform(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
 }
