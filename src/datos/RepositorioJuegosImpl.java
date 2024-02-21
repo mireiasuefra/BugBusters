da83113ee08fb76d6749abc524e04e55c4e7f7f1
AdrianMontes <adrian.montes01@estudiant.upf.edu>
2024-02-21 21:26:44 +0100

    Arreglado la separación de comas para imprimir los nombres

--- src/datos/RepositorioJuegosImpl.java
+++ src/datos/RepositorioJuegosImpl.java
@@ -23,7 +23,7 @@ public class RepositorioJuegosImpl implements RepositorioJuegos {
 	public int cargarDatosCSV() {
 
 		int longCSV = 0;
-		try (Scanner scanner = new Scanner(new File("res/juegos.csv"))) {
+        try (Scanner scanner = new Scanner(new File("res/juegos.csv"))) {
 			scanner.nextLine();
 			while (scanner.hasNextLine()) {
 				listado.add(leerJuegoString(scanner.nextLine()));
@@ -38,17 +38,35 @@ public class RepositorioJuegosImpl implements RepositorioJuegos {
 	}
 
 	private Juego leerJuegoString(String linea) {
-
-//		String[] datosJuego = linea.split(",(?!\\s)");
-		
-
 		Juego juego = new Juego();
 		int fecha = 0;
 		
-		
-		if(!linea.contains("\"")) {
+		if (linea.startsWith("\"")) {
+			//Primero separamos el nombre
+			String[] datos = linea.split("\"\"");
+			String[] datosNombre = datos[1].split(",");
+			String nombre = "";
+			for(int i = 0; i < datosNombre.length; i++) {
+				nombre += datosNombre[i];
+			}
+			juego.setNombre(nombre);
+			
+			//Resto de datos
+			String[] restoDatos = datos[2].split(",");
+			juego.setPlataforma(Platform.fromString(restoDatos[1]));
+			try {
+				fecha = Integer.parseInt(restoDatos[2]);
+			} catch (NumberFormatException e) {
+				e.getMessage();
+			} finally {
+				juego.setFechaPublicacion(fecha);
+			}
+			
+			juego.setGenero(Genre.fromString(restoDatos[3]));
+			juego.setEditor(restoDatos[4]);
+			//System.out.println(restoDatos[2]);
+		} else {
 			String[] datosJuego = linea.split(",");
-			datosJuego = linea.split(",");
 			juego.setNombre(datosJuego[1]);
 			juego.setPlataforma(Platform.fromString(datosJuego[2]));
 			try {
@@ -61,8 +79,33 @@ public class RepositorioJuegosImpl implements RepositorioJuegos {
 
 			juego.setGenero(Genre.fromString(datosJuego[4]));
 			juego.setEditor(datosJuego[5]);
-		}		
+		}
+		
+		return juego;
+		/*
+		Juego juego;
+		if(linea.contains("\"\"")) {
+			juego = new Juego("null", 0, "null", Platform.NULL, Genre.NULL);
+			return juego;
+		}
+		//System.out.println(linea);
+		
+		juego = new Juego();
+		int fecha = 0;
+		String[] datosJuego = linea.split(",");
+		juego.setNombre(datosJuego[1]);
+		juego.setPlataforma(Platform.fromString(datosJuego[2]));
+		try {
+			fecha = Integer.parseInt(datosJuego[3]);
+		} catch (NumberFormatException e) {
+			e.getMessage();
+		} finally {
+			juego.setFechaPublicacion(fecha);
+		}
 
+		juego.setGenero(Genre.fromString(datosJuego[4]));
+		juego.setEditor(datosJuego[5]);
+		++longCSV;
 //		String[] datosJuego = linea.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
 //		String modifiedLine = linea.replaceAll("(?<!\")\\,", "_");
 
@@ -78,10 +121,8 @@ public class RepositorioJuegosImpl implements RepositorioJuegos {
 //		System.out.println(datosJuego[3]);
 //		System.out.println(datosJuego[4]);
 //		System.out.println(datosJuego[5]);
-
-		
 		return juego;
-
+		*/
 	}
 
 	@Override
--- src/model/Platform.java
+++ src/model/Platform.java
@@ -3,8 +3,9 @@ package model;
 public enum Platform {
 
 	WII("Wii"), NES("NES"), GB("GB"), DS("DS"), X360("X360"), PS3("PS3"), PS2("PS2"), SNES("SNES"), GBA("GBA"),
-	_3DS("3DS"), PS4("PS4"), N64("N64"), PS5("PS5"), PC("PC"), _2600("2600"), PSP("PSP"), XONE("XOne"),
-	GC("GC"), WIIU("WiiU"), GEN("GEN"), PS("PS"), XB("XB"), DC("DC"), PSV("PSV"), SAT("SAT"), SCD("SCD");
+	_3DS("3DS"), PS4("PS4"), N64("N64"), PS5("PS5"), PC("PC"), _2600("2600"), PSP("PSP"), XONE("XOne"), WS("WS"),
+	GC("GC"), WIIU("WiiU"), GEN("GEN"), PS("PS"), XB("XB"), DC("DC"), PSV("PSV"), SAT("SAT"), SCD("SCD"), NG("NG"),
+	TG16("TG16"), _3DO("3DO"), GG("GG"), PCFX("PCFX"), NULL("null");
 
 	private final String plataforma;
 
