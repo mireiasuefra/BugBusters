5891b48c916a4f612aafb84f523ec93164307192
AdrianMontes <adrian.montes01@estudiant.upf.edu>
2024-02-21 12:11:46 +0100

    Creado el método seleccionarOpciones

--- src/control/GestionJuegos.java
+++ src/control/GestionJuegos.java
@@ -1,7 +1,11 @@
 package control;
 
+import java.util.List;
+
+import model.Juego;
 import service.GestionJuegosService;
 import service.GestionJuegosServiceImpl;
+import utilidades.Utilidades;
 
 public class GestionJuegos {
 
@@ -9,8 +13,33 @@ public class GestionJuegos {
 	
 
 	public void cargarDatos() {
-		
 		gestionJuegosService.cargarDatosCSV();
 	}
-
+	
+	public void seleccionarOpciones() {
+		int opcion = 0;
+		
+		do {
+			GUI.mostrarMenu();
+			opcion = Utilidades.escribirInt("Elige una opcion: ");
+			
+			switch(opcion) {
+			case 1: //Listar juegos
+				List<Juego> lista = gestionJuegosService.listarJuegos();
+				GUI.mostrarListaJuegos(lista);
+				break;
+			case 0: //Salir del programa
+				System.out.println("Saliendo del programa...");
+				try {
+					Thread.sleep(3000);
+				} catch (Exception e) {
+					System.out.println("Ha habido un problema al intentar salir del programa.");
+				}
+				break;
+			default: //Si no se marca una opcion correcta
+				System.out.println("No has elegido una opcion correcta.");
+				break;
+			}
+		} while(opcion != 0);
+	}
 }
